4c1238902275e2622d0971b012c5fae09768dbde
BT_3W2_17 <donniem@nycstudents.net>
2022-09-28 11:37:38 -0400

    added two print statements

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("yo sup");
+System.out.println("did you know that frogs are amphibians?");
+System.out.println("and also the fact that wood comes from trees?");
+}
 }
